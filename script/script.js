ff8c00e00d7f492e8fda6756982058a894fdeea3
S.M ANZUMUL JUBAYER <anzumuljubayer222@gmail.com>
2025-08-27 16:20:31 +0600

    coin-funtionality

--- script/script.js
+++ script/script.js
@@ -1,1 +1,22 @@
-console.log("connected")
+// resuseable function
+
+function getId(id){
+    const elements=document.getElementById(id)
+    return elements
+}
+
+const callbtns=document.getElementsByClassName('call-btn ')
+// call function
+ for(const callbtn of callbtns){
+    callbtn.addEventListener('click',function(event){
+        
+        const coinElement=getId('coin-count')
+        const coinValue= Number(coinElement.innerText)
+        if(coinValue<20){
+         return alert("আপনার পর্যাপ্ত কয়েন নেই")
+        }
+        const uCoinValue=coinValue-20
+        coinElement.innerText=uCoinValue
+    })
+ }
+ 
